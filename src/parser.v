59dd6e4edb30373be4a2f84af387678d0df4ca22
bingsoo420 <gearmoss+bingsoo@gmail.com>
2023-09-08 18:36:45 +0100

    parser: `thread` is a reserved keyword

--- src/parser.v
+++ src/parser.v
@@ -8,7 +8,7 @@ fn parse_general(title string) (bool, string) {
 	mut re := regex.regex_opt(re_general) or { return false, '' }
 	start, end := re.find(title)
 	if start != -1 && end != -1 {
-		return true, title[start + 1..end - 1].to_lower()
+		return true, title[start+1..end-1].to_lower()
 	}
 	return false, ''
 }
@@ -16,10 +16,10 @@ fn parse_general(title string) (bool, string) {
 fn build_generals(board string, catalog []Page) map[string]string {
 	mut general := map[string]string{}
 	for page in catalog {
-		for thread in page.threads {
-			found, gen := parse_general(thread.title)
+		for t in page.threads {
+			found, gen := parse_general(t.title)
 			if found {
-				general[gen] = 'https://boards.4chan.org/${board}/thread/${thread.no}'
+				general[gen] = 'https://boards.4chan.org/${board}/thread/${t.no}'
 			}
 		}
 	}
